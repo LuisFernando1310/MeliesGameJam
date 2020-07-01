87b33d3a119720896223d31c05ca7845a1a68954
Heyviics <45526503+Heyviics@users.noreply.github.com>
2020-07-01 03:07:53 -0300

    textos e fontes
    tentativa animacao camera

---
+++ MeliesGameJamProject/Assets/Creditscamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creditscamera : MonoBehaviour
+{
+    public Animation anim;
+   
+
+    public void CameraVai()
+    {
+        anim.Play("CameraCredits");
+    }
+}
