e84bca7ed59c808cb8d96956a1c10bacea3c882d
Heyviics <45526503+Heyviics@users.noreply.github.com>
2020-07-01 16:04:10 -0300

    cutscene inicial ok

---
+++ MeliesGameJamProject/Assets/Images/FalasCutScenes.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FalasCutScenes : MonoBehaviour
+{
+
+    public GameObject luisaf1;
+    public GameObject luisaf2;
+    public GameObject jorgef1;
+    public GameObject jorgef2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        luisaf1.SetActive(true);
+        luisaf2.SetActive(false);
+        jorgef1.SetActive(false);
+        jorgef2.SetActive(false);
+        Invoke("Jorge1", 5f);
+    }
+
+    public void Jorge1()
+    {
+        luisaf1.SetActive(false);
+        luisaf2.SetActive(false);
+        jorgef1.SetActive(true);
+        jorgef2.SetActive(false);
+        Invoke("Luisa2", 5f);
+    }
+
+    public void Luisa2()
+    {
+        luisaf1.SetActive(false);
+        luisaf2.SetActive(true);
+        jorgef1.SetActive(false);
+        jorgef2.SetActive(false);
+        Invoke("Jorge2", 5f);
+    }
+
+    public void Jorge2()
+    {
+        luisaf1.SetActive(false);
+        luisaf2.SetActive(false);
+        jorgef1.SetActive(false);
+        jorgef2.SetActive(true);
+        Invoke("Apaga", 5f);
+    }
+
+    public void Apaga()
+    {
+        luisaf1.SetActive(false);
+        luisaf2.SetActive(false);
+        jorgef1.SetActive(false);
+        jorgef2.SetActive(false);
+        Invoke("StartGame", 3f);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ MeliesGameJamProject/Assets/Images/text.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class text : MonoBehaviour
+{
+    public Text txt;
+    public string textoFala;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(ditado(textoFala));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator ditado(string frase)
+    {
+        int letra = 0;
+        txt.text = "";
+
+        while (letra < frase.Length)
+        {
+            txt.text += frase[letra];
+            yield return new WaitForSeconds(DefineTempoDeEspera(frase[letra]));
+            letra += 1;
+            //yield return null;
+        }
+    }
+
+    float DefineTempoDeEspera(char caractere)
+    {
+        switch (caractere)
+        {
+            case ',':
+            case '.':
+                return 0.3f;
+            case ' ':
+                return 0.06f;
+            case '!':
+            case '?':
+                return 0.5f;
+            default:
+                return 0.03f;
+        }
+    }
+}
