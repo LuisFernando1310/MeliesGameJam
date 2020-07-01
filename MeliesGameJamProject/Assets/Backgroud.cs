adf44467c3cbb171c610965e63b5827df473b839
Heyviics <45526503+Heyviics@users.noreply.github.com>
2020-07-01 02:22:27 -0300

    post processing menu

---
+++ MeliesGameJamProject/Assets/Backgroud.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backgroud : MonoBehaviour
+{
+
+    public MeshRenderer image;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
+    }
+}
