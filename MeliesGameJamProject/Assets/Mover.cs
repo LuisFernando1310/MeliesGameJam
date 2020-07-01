4a223a3bfb983c7e1d78c20e9fcb1399aca78dd1
luisFernando <luisfs1310@gmail.com>
2020-07-01 10:17:45 -0300

    Criação da movimentação dos dois jogadores

---
+++ MeliesGameJamProject/Assets/Mover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    private float MoveSpeed = 5f;
+
+    [SerializeField]
+    int playerIndex = 0;
+
+    private CharacterController controller;
+
+    Vector3 moveDirection = Vector3.zero;
+    Vector2 inputVector = Vector2.zero;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    public int GetPlayerIndex()
+    {
+        return playerIndex;
+    }
+
+    public void SetInputVector(Vector2 direction)
+    {
+        inputVector = direction;
+    }
+
+    private void Update()
+    {
+        moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        controller.Move(moveDirection * MoveSpeed * Time.deltaTime);
+    }
+}
