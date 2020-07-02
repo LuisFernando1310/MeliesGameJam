11a4d8baa84fb327fbd597b39ca9ab19b63ca85d
Heyviics <45526503+Heyviics@users.noreply.github.com>
2020-07-02 18:35:06 -0300

    Agora anda porra

--- MeliesGameJamProject/Assets/Animations/Jorge/JorgeOK.cs
+++ MeliesGameJamProject/Assets/Animations/Jorge/JorgeOK.cs
@@ -2,20 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LuisaController : MonoBehaviour
+public class JorgeOK : MonoBehaviour
 {
+
     public CharacterController controller;
     public float speed = 5f;
 
-
+    // Update is called once per frame
     void Update()
     {
-
-        float horizontal = Input.GetAxisRaw("Horizontal2");
-        float vertical = Input.GetAxisRaw("Vertical2");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
 
-        if (direction.magnitude >= 0.1f)
+        if(direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, targetAngle, 0);
--- MeliesGameJamProject/Assets/Scripts/playersVic/JorgeController.cs
+++ MeliesGameJamProject/Assets/Scripts/playersVic/JorgeController.cs
@@ -6,18 +6,39 @@ public class JorgeController : MonoBehaviour
 {
     public float speed = 20;
     private Vector3 pos;
+    private Vector3 direction;
+    public bool poderAtivado = false;
 
     private void Start()
     {
+       
 
     }
 
     void Update()
     {
 
+        Movimentacao();
+        AtivaPoder();
+    }
+
+    void AtivaPoder()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            poderAtivado = true;
+        }
+
+        
+    }
+
+    void Movimentacao()
+    {
         pos = transform.position;
         pos.x += speed * Input.GetAxis("Horizontal2") * Time.deltaTime;
         pos.z += speed * Input.GetAxis("Vertical2") * Time.deltaTime;
         transform.position = pos;
+        //Quaternion target = Quaternion.Euler(pos.x, 0, pos.z);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * speed);
     }
 }
