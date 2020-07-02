41c011f21be911d04a9ade811bb8ed17daeee3aa
Heyviics <45526503+Heyviics@users.noreply.github.com>
2020-07-02 11:53:56 -0300

    barra de medo e coletar chaves ok

---
+++ MeliesGameJamProject/Assets/Scripts/playersVic/Coletaveis.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coletaveis : MonoBehaviour
+{
+    public int chave = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Luisa")
+        {
+            this.gameObject.SetActive(false);
+            chave++;
+        }
+
+        
+    }
+}
